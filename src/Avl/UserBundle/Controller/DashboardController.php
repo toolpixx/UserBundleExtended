4d36832e210d98bc483461c4415713e47a78edeb
Andreas van Loock <a.vanloock@toolpixx.com>
2015-01-23 16:06:26 +0100

    Add cache for rss-feeds

--- src/Avl/UserBundle/Controller/DashboardController.php
+++ src/Avl/UserBundle/Controller/DashboardController.php
@@ -28,6 +28,7 @@ class DashboardController extends Controller
             'user' => $this->getUser(),
             'symfonyRss' => $this->getRssFeed('http://feeds.feedburner.com/symfony/blog'),
             'stackoverflowRss' => $this->getRssFeed('http://stackoverflow.com/feeds/tag/php+symfony'),
+            'scoopRss' => $this->getRssFeed('http://www.scoop.it/t/webdevilopers/rss.xml'),
             'githubRss' => $this->getRssFeed('https://github.com/symfony/symfony/commits/master.atom'),
         ));
     }
@@ -35,14 +36,43 @@ class DashboardController extends Controller
     /**
      * Get a rssfeed
      *
-     * Todo: make it better (errorhandling)
+     * Todo: make it better (errorhandling and caching)
      *
      * @param $url
      * @return \SimpleXMLElement
      */
     private function getRssFeed($url) {
         try {
-            return simplexml_load_file($url);
+            /**
+             * CacheKey
+             */
+            $cache_key = $url;
+
+            /**
+             * Get the cache-driver
+             */
+            $cache_driver = $this->container->get('liip_doctrine_cache.ns.rssfeed');
+
+            /**
+             * If content was cached return it
+             */
+            if ($cache_driver->contains($cache_key)) {
+
+                return
+                    simplexml_load_string($cache_driver->fetch($cache_key));
+            }
+            /**
+             * Or get and save the new content
+             */
+            else {
+                $rssFeed = file_get_contents($url);
+                /**
+                 * Save to cache ad return content
+                 */
+                $cache_driver->save($cache_key, $rssFeed, strtotime('5 Hours'));
+                return simplexml_load_string($rssFeed);
+            }
+
         } catch(ContextErrorException $e) {
             $this->get('logger')->error('Can not load: '.$url);
             $this->get('logger')->error($e->getCode().' : '.$e->getMessage());
